b4dcdf84e09c3b7da176a87590e8884759ff2fa1
felixshiftellecon <nick.smith@circleci.com>
2021-08-11 13:59:03 -0400

    add centralized error handling and reverted deliberate error

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,7 +11,7 @@ import (
 // *application
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper.
 		return
 	}
 
@@ -23,20 +23,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		// Because the home handler function is now a method against application
-		// it can access its fields, including the error logger. We'll write the log
-		// message to this instead of the standard logger.
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Use serverError() helper.
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		// Also update the code here to use the error logger fro the application
-		// struct.
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Use serverError() helper.
 	}
 }
 
@@ -45,7 +38,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use notFound() helper.
 		return
 	}
 
@@ -57,7 +50,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper
 		return
 	}
 
