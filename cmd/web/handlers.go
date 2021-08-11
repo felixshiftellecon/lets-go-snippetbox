2614fb9e69401c93415e56c08eb1bdec1accf593
felixshiftellecon <nick.smith@circleci.com>
2021-08-11 14:31:02 -0400

    move routes to new file remove all chapter 3 comments

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,11 +7,9 @@ import (
 	"strconv"
 )
 
-// Change the signature of the home handler so it is defined as a method against
-// *application
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w) // Use the notFound() helper.
+		app.notFound(w)
 		return
 	}
 
@@ -23,34 +21,30 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.serverError(w, err) // Use serverError() helper.
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.serverError(w, err) // Use serverError() helper.
+		app.serverError(w, err)
 	}
 }
 
-// Change the signature of the showSnippet handler so it is defined as a method
-// against *application.
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		app.notFound(w) // Use notFound() helper.
+		app.notFound(w)
 		return
 	}
 
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-// Change the signature of the createSnippet handler so it is defined as a method
-// against *application.
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
