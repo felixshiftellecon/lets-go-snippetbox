f8902ebc796970667edd6a1d0ea7d6ad4fd508dd
felixshiftellecon <nick.smith@circleci.com>
2021-08-12 16:47:18 -0400

    add showSnippet html template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 
-	// "html/template"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -62,7 +62,30 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	// Create an instance of a templateData struct holding the snippet data.
+
+	data := &templateData{Snippet: s}
+
+	// Initialize a slice containing the paths to the show.page.tmpl file,
+	// plus the base layout and footer partial that we made earlier.
+	files := []string{
+		"./ui/html/show.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	// Parse the template files...
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	// Pass in the templateData struct when executing the template
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
