807d8d83df077a313e176fe5abee72b8a7d7357a
felixshiftellecon <nick.smith@circleci.com>
2021-08-16 15:39:35 -0400

    add alice middleware chain

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // Update the signature for the routes() method so that it returns a
 // http.Handler instead of *http.ServeMux
 func (app *application) routes() http.Handler {
+	//Create a middleware chain containing our "standard" middleware
+	// which will be used for every request our application receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -13,10 +21,6 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Pass the servemux as the "next" parameter  to the secureHeaders middleware.
-	// Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	// Wrap the existing chain with the logRequest middleware.
-	// Wrap it again with the recoverPanic middleware
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// Return the standardMiddleware chain
+	return standardMiddleware.Then(mux)
 }
