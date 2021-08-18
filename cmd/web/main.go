fe409338c16c611300833393f2735427d233747f
felixshiftellecon <nick.smith@circleci.com>
2021-08-18 09:33:48 -0400

    remove chapter 10 comments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -47,7 +47,7 @@ func main() {
 
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = 12 * time.Hour
-	session.Secure = true // Set the Secure flag on our session cookies
+	session.Secure = true
 
 	app := &application{
 		errorLog:      errorLog,
@@ -57,30 +57,22 @@ func main() {
 		templateCache: templateCache,
 	}
 
-	// Initialize a tls.Config struct to hold the non-default TLS settings we want
-	// the server to use.
 	tlsConfig := &tls.Config{
 		PreferServerCipherSuites: true,
 		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256},
 	}
 
-	// Set the service's TLSConfig field to use the tlsConfig variable we just
-	// created.
 	srv := &http.Server{
-		Addr:      *addr,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
-		// Add Idle, Read and Write timeouts to the server.
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	// Use the ListenAndServeTLS() method to start the HTTPS server. We
-	// pass in the paths to the TLS certificate and corresponding private key as
-	// the two parameters.
 	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
